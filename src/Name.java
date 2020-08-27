d39b33a0cb9d8b753e3a9a88ce3848b9fb2752db
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-27 14:21:42 -0500

    exercises

--- src/Name.java
+++ src/Name.java
@@ -10,27 +10,13 @@ import java.util.Scanner;
 public class Name  {
 
     static Scanner scan = new Scanner(System.in);
-    static String userInput = scan.nextLine();
-   // Name n = new Name();
 
     public static Path p = Paths.get("./src/contacts.txt").normalize();
 
     //    creating or declaring  variables
     List<Contacts> contacts;
 
-
-//read the contents of the instructor-names.txt file and store the list of strings into the 'names' variable
-
-    public static void main(String[] args) {
-        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
-        System.out.println("1. View contacts. ");
-        System.out.println("2. Add a new contact.");
-        System.out.println("3. Search a contact by name.");
-        System.out.println("4. Delete an existing contact.");
-        System.out.println("5. Exit.");
-
-
-        //read out the file
+    public  static void readAll(){
         try {
             List<String> lines = Files.readAllLines(p);
             System.out.println("Name       | Phone number |");
@@ -38,21 +24,62 @@ public class Name  {
             for (String line : lines) {
                 System.out.println(line);
             }
+            System.out.println("_____________");
         } catch (
                 IOException e) {
             e.printStackTrace();
         }
+    }
 
-//        addNewContacts();
+
+//read the contents of the instructor-names.txt file and store the list of strings into the 'names' variable
+
+    public static void main(String[] args) {
+        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+        System.out.println("1.   View contacts. ");
+        System.out.println("2.   Add a new contact.");
+        System.out.println("3.   Search a contact by name.");
+        System.out.println("4.   Delete an existing contact.");
+        System.out.println("5.   Exit.");
+
+
+        //read out the file
+//        try {
+//               List<String> lines = Files.readAllLines(p);
+//            System.out.println("Name       | Phone number |");
+//            System.out.println("***************************");
+//            for (String line : lines) {
+//                System.out.println(line);
+//            }
+//            System.out.println("_____________");
+//        } catch (
+//                IOException e) {
+//            e.printStackTrace();
+//        }
+
+        readAll();
+        //calling the methods for adding contacts
+        addNewContacts();
 
     }
 
     public static void addNewContacts() {
+
         try {
-            Files.write(p, Arrays.asList(userInput), StandardOpenOption.APPEND);
+            System.out.println("To add New contacts ");
+            System.out.println("please enter your name  ");
+            String addName = scan.nextLine() ;
+            System.out.println("please enter your number  ");
+            String addNumber = scan.nextLine() ;
+            System.out.println("The new contact list will be : \n");
+
+            Files.write(p, Arrays.asList(String.format("%s | %s", addName, addNumber)),StandardOpenOption.APPEND);
+            readAll();
         } catch (IOException e) {
             //exception handling left as an exercise for the reader
+            System.out.println("something went wrong");
         }
+
     }
 
 }
