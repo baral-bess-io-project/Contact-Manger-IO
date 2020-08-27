63b72506b98c97961697c7d18030f1893f249f1d
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-27 10:43:12 -0500

    exercises

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,5 +1,48 @@
+
+
+import java.util.Scanner;
+
 public class Contacts {
+    public String names;
+    public String phoneNumbers;
+
+    //constructor of class
+    public Contacts(String names, String phoneNumbers) {
+        this.names = names;
+        this.phoneNumbers = phoneNumbers;
+    }
+
+// getter and setter for names, phone numbers
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(String phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+
+//    creating path for contact.txt
+
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+        System.out.println("1. View contacts. ");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit.");
+        Scanner scan = new Scanner(System.in);
+        int userInput = scan.nextInt();
+        System.out.println(userInput);
+
+
     }
 }
